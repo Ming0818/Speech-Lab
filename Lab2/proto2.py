c7f3ec6600bb40f6d70122094e73e7b9618bb271
MandyXue <13564039693@163.com>
2018-04-19 23:57:34 +0200

    finish 4.1 4.2

--- Lab2/proto2.py
+++ Lab2/proto2.py
@@ -1,6 +1,8 @@
 import numpy as np
 from tools2 import *
 from prondict import *
+from sklearn.mixture import *
+import matplotlib.pyplot as plt
 
 def concatHMMs(hmmmodels, namelist):
     """ Concatenates HMM models in a left to right manner
@@ -26,9 +28,9 @@ def concatHMMs(hmmmodels, namelist):
     """
     new_H = {}
     # M = 3, D = 13
-    M = phoneHMMs['sil']['means'].shape[0]
-    D = phoneHMMs['sil']['means'].shape[1]
-    num_tran = phoneHMMs['sil']['transmat'].shape[0]
+    M = hmmmodels['sil']['means'].shape[0]
+    D = hmmmodels['sil']['means'].shape[1]
+    num_tran = hmmmodels['sil']['transmat'].shape[0]
 
     # 数量
     pho_num = len(namelist)
@@ -36,15 +38,18 @@ def concatHMMs(hmmmodels, namelist):
     new_trans = np.zeros((pho_num *(M+1)-2,pho_num *(M+1)-2))
     new_means = np.zeros((M * pho_num, D))
     new_covars = np.zeros(new_means.shape)
+    new_startprob = np.zeros(pho_num*3)
+    new_startprob[0] = 1.0
 
     for i in range(pho_num):
-        hmm = phoneHMMs[namelist[i]]
+        hmm = hmmmodels[namelist[i]]
         new_trans[i*M:i*M+num_tran, i*M:i*M+num_tran] = hmm['transmat']
         new_means[i*M:(i+1)*M] = hmm['means']
         new_covars[i*M:(i+1)*M] = hmm['covars']
     new_H.update({'transmat':new_trans})
     new_H.update({'means': new_means})
-    new_H.update({'covars':new_covars})
+    new_H.update({'covars': new_covars})
+    new_H.update({'startprob': new_startprob})
 
     return new_H
 
@@ -73,6 +78,17 @@ def forward(log_emlik, log_startprob, log_transmat):
     Output:
         forward_prob: NxM array of forward log probabilities for each of the M states in the model
     """
+    N = log_emlik.shape[0]  # 71
+    M = log_emlik.shape[1]  # 9
+    logalpha = np.zeros(log_emlik.shape)
+    for n in range(N):
+        for j in range(M):
+            if n == 0:
+                logalpha[n][j] = log_startprob[j] + log_emlik[n][j]
+            else:
+                logalpha[n][j] = logsumexp(logalpha[n-1]+log_transmat.T[j][0:M]) + log_emlik[n][j]
+    return logalpha
+
 
 def backward(log_emlik, log_startprob, log_transmat):
     """Backward (beta) probabilities in log domain.
@@ -129,12 +145,12 @@ def updateMeanAndVar(X, log_gamma, varianceFloor=5.0):
 
 
 if __name__== "__main__":
-    data = np.load('lab2_data.npz')['data']
-    phoneHMMs = np.load('lab2_models.npz')['phoneHMMs'].item()
+    data = np.load('data/lab2_data.npz')['data']
+    phoneHMMs = np.load('data/lab2_models.npz')['phoneHMMs'].item()
     list(sorted(phoneHMMs.keys()))
     phoneHMMs['ah'].keys()
 
-    example = np.load('lab2_example.npz')['example'].item()
+    example = np.load('data/lab2_example.npz')['example'].item()
     list(example.keys())
 
     prondict = prondict_list()
@@ -145,3 +161,22 @@ if __name__== "__main__":
 
     wordHMMs = {}
     wordHMMs['o'] = concatHMMs(phoneHMMs, modellist['o'])
+
+    # 4.1 Gaussian emission probabilities
+    # verify = example['obsloglik']
+    # result = log_multivariate_normal_density_diag(example['lmfcc'], wordHMMs['o']['means'], wordHMMs['o']['covars'])
+    # print((abs(verify - result) < 0.0000001).all())
+    #
+    # plt.pcolormesh(result.T)
+    # plt.show()
+
+    # 4.2 forward algorithm
+    verify = example['logalpha']
+    result = forward(example['obsloglik'], np.log(wordHMMs['o']['startprob']), np.log(wordHMMs['o']['transmat']))
+    print((abs(verify - result) < 0.0000001).all())
+    # 这个不知道是不是有inf值的关系 没法比较 然后pcolormesh打不出来 所以用了imshow
+    # plt.subplot(2, 1, 1)
+    # plt.imshow(verify.T)
+    # plt.subplot(2, 1, 2)
+    plt.imshow(result.T)
+    plt.show()
